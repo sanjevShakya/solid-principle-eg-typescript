0d87931e54f5f455875b28906e3ae7aef3e3cdb7
Sanjeev Shakya <sanjevsakya@gmail.com>
2020-01-20 16:54:05 +0545

    add single reponsibility example in typescript

---
+++ src/single-responsibility/bookPrinter.ts
@@ -0,0 +1,9 @@
+export class BookPrinter {
+  printToConsole(text: string): void {
+    console.log('book text', text);
+  }
+
+  printTextToFile(text: string): void {
+    // Code impl to write the text to the file.
+  }
+}
